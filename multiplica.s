46ff29f7d1a841b242f3011cbb6c57b07fadd4ee
RitaRez <ritarezborges@gmail.com>
2021-01-05 22:40:32 -0300

    multiplica funciona

--- multiplica.s
+++ multiplica.s
@@ -1,27 +1,34 @@
-multiplica:
-    beq x10, x0, END # if( a == 0| |b == 0)
-    beq x11, x0, END 
+addi x12, x0, 3
+addi x13, x0, -3
+jal x1, multiplica
+addi x12, x0, -3 #a
+mv x13, x10 #b
+jal x1, multiplica
+jal x1, END
 
+multiplica:
     addi x5, x0, 0 # int  resul = 0;
-    blt x5, x0, L1 # if(a < 0)
+
+    beq x12, x0, RETURN # if( a == 0| |b == 0)
+    beq x13, x0, RETURN 
+
+    bge x12, x0, LOOP2 # if(a < 0)
     
     LOOP1:
-        beq x10, x0, L1
-        sub x5, x5, x11 # resul = resul - b 
-        addi x10, x10, 1 # a++
-        beq x0, 0, LOOP1
+        beq x12, x0, RETURN
+        sub x5, x5, x13 # resul = resul - b 
+        addi x12, x12, 1 # a++
+        beq x0, x0, LOOP1 #loop
     
-    L1:
-        LOOP2:
-            beq x10, x0, L2
-            add x5, x5, x11 # resul = resul + b 
-            addi x10, x10, -1 # a--
-            beq x0, 0, LOOP2
     
-    L2:
+    LOOP2:
+        beq x12, x0, RETURN
+        add x5, x5, x13 # resul = resul + b 
+        addi x12, x12, -1 # a--
+        beq x0, x0, LOOP2 #loop
+    
+    RETURN:    
+        mv x10, x5
         jalr x0, 0(x1)
-
-
-
+    
 END:
-    jalr x0, 0(x1)
